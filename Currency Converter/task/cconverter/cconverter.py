a83b386b2edaf85c6fbfc6f1342b095f83e0400f
Korwin <korwin-bieniek@wp.pl>
2022-01-01 18:03:44 +0100

    Simple Currency Converter

---
+++ Currency Converter/task/cconverter/cconverter.py
@@ -0,0 +1,5 @@
+# write your code here!
+conicoins = int(input())
+print(f'I have {conicoins} conicoins')
+print(f'{conicoins} conicoins cost {conicoins * 100} dollars.')
+print('I am rich! Yippee!')
